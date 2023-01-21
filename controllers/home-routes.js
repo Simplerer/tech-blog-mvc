550c150ad075cd96b63b20f7bdb1eb1e4b8ae352
A Stone <simplererstone@gmail.com>
2023-01-20 23:22:24 -0500

    set up seed data to test rendering on homepage and now render posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,12 +4,9 @@ const { User, Comment, Post } = require('../models/');
 // get all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      
-    })
-    console.log(postData);
-    // res.json(postData)
-    res.render('homepage');
+    const postData = await Post.findAll({});
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('homepage', { posts });
   } catch (err) {
     res.status(500).json(err);
   }
